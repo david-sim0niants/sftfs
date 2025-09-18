1340d7dbb494de7aec5eb4d2e4cbba6e2e4ba0cf
david-sim0niants <david.simonyants@gmail.com>
2025-09-19 00:28:33 +0400

    sftfs_getattr() initial impl

--- src/main.c
+++ src/main.c
@@ -1,35 +1,25 @@
+#include "core.h"
 #include "logging.h"
 
 #define FUSE_USE_VERSION 31
 #include <fuse.h>
-#include <libssh/sftp.h>
-#include <libssh/libssh.h>
 
-struct sftfs_handle {
-    ssh_session session;
-} handle;
+// static void sftfs_init(void)
+// {
+//     handle.session = ssh_new();
+//     if (handle.session == NULL)
+//         sftfs_error("Failed creating SSH session\n");
+// }
 
-static void sftfs_init(void)
-{
-    handle.session = ssh_new();
-    if (1 || handle.session == NULL)
-        sftfs_error("Failed creating SSH session\n");
-}
-
-static int sftfs_getattr(const char *path, struct stat *stat)
-{
-    return 0;
-}
-
-static void sftfs_destroy(void *data)
-{
-    struct sftfs_handle *handle = (struct sftfs_handle *)(data);
-    ssh_free(handle->session);
-}
+// static void sftfs_destroy(void *data)
+// {
+//     struct sftfs_handle *handle = (struct sftfs_handle *)(data);
+//     ssh_free(handle->session);
+// }
 
 static struct fuse_operations ops = {
     .getattr = sftfs_getattr,
-    .destroy = sftfs_destroy,
+    // .destroy = sftfs_destroy,
 };
 
 // struct sftfs_config {
@@ -37,8 +27,8 @@ static struct fuse_operations ops = {
 //
 int main(int argc, char *argv[])
 {
-    // struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-    sftfs_init();
+    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+    // sftfs_init();
 
-    return fuse_main(argc, argv, &ops, &handle);
+    return fuse_main(argc, argv, &ops, NULL);
 }
